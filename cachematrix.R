0d3252c5fed04809672f939a6b4dff9f2374985a
alextirasp <alxtirasp@gmail.com>
2015-10-21 16:20:20 -0400

    Programming Assignment 2 (R programming course)
    Working code for caching inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Get inverse of the matrix from cache  
+## or calculate the inverse matrix and cache the result if it was not cached yet
 
-## Write a short comment describing this function
+## This function prepares a vector of functions to set/get matrix in/from cache, 
+## and set/get inverse matrix in/from cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  inv_mx<-matrix()
+  
+  set<-function(y) {
+    x<<-y
+    inv_mx<<-matrix()
+  }
+  
+  get<-function() x
+  
+  setinverse<-function(new_inv_mx) {
+    inv_mx<<-new_inv_mx
+  } 
+  
+  getinverse<-function() {
+    inv_mx
+  }
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse matrix for matrix in argument
+## was already calculated and cached, it returnes the cached matrix, 
+## or if the cached matrix only element is NA (matrix initialized as empty), 
+## it calculates inverse matrix and caches the result then returns it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+
+  res_mx<-x$getinverse()
+  
+  if((ncol(res_mx)>1)|(!is.na(res_mx[1,1]))) {
+    message("Cached result returned")
+    return(res_mx)
+  }
+    
+    arg_mx<-x$get()
+    res_mx<-solve(arg_mx)
+    x$setinverse(res_mx)
+      message("Calculated result returned")
+    res_mx
 }
